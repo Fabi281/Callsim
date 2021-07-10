4bb7a67bc544a165c458515f713369126916e9f3
Bernd Mayinger <berndmayinger@gmail.com>
2021-07-10 10:44:45 +0200

    simple chat app found and working now

---
+++ test_vom_Bernd/api/DateServer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * A simple TCP server. When a client connects, it sends the client the current
+ * datetime, then closes the connection. This is arguably the simplest server
+ * you can write. Beware though that a client has to be completely served its
+ * date before the server will be able to handle another client.
+ */
+public class DateServer {
+    public static void main(String[] args) throws IOException {
+        try (var listener = new ServerSocket(59090)) {
+            System.out.println("The date server is running...");
+            while (true) {
+                try (var socket = listener.accept()) {
+                    var out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                }
+            }
+        }
+    }
+}
