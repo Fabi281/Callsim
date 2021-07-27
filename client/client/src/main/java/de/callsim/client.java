6500f566d1758b4c96e492fcbe965ebb8e45fef5
Bernd Mayinger <berndmayinger@gmail.com>
2021-07-27 20:29:24 +0200

    login form designed so far

--- client/client/src/main/java/de/callsim/client.java
+++ client/client/src/main/java/de/callsim/client.java
@@ -1,18 +1,48 @@
 package de.callsim;
 
 
+import java.awt.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.swing.*;
 
 public class client {
 
     public static void main(String[] args) {
+
+        final boolean showLogin = false;
+
+        if(showLogin){
+            JFrame frame = new JFrame("CallSim Login");
+            frame.setContentPane(new Login().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setMinimumSize(new Dimension(500,400));
+            frame.pack();
+            frame.setVisible(true);
+        }
+        else{
+            JFrame frame = new JFrame("App");
+
+            frame.setContentPane(new NextWindow().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setMinimumSize(new Dimension(500, 200)); // ref: https://stackoverflow.com/a/2782041
+            frame.pack();
+            frame.setVisible(true);
+        }
+
+
+
+    }
+
+    public void startConnection(){
+
         try {
             // open websocket
             final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://localhost:8787/ws"));
+
 
             // add listener
             clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
@@ -47,7 +77,7 @@ public class client {
 
             System.console().readLine();
 
-        }catch (URISyntaxException ex) {
+        } catch (URISyntaxException ex) {
             System.err.println("URISyntaxException exception: " + ex.getMessage());
         }
     }
