a80c66af8273b0fd123c52e4ed3155b54b5fe55b
Fabi281 <Fabian.Meindl@freenet.de>
2021-10-08 12:28:00 +0200

    Introduced File-Logging as per requirement (Not 100% tested)

--- server/src/main/java/de/dhbwheidenheim/informatik/callsim/CallSimApplication.java
+++ server/src/main/java/de/dhbwheidenheim/informatik/callsim/CallSimApplication.java
@@ -3,11 +3,14 @@ package de.dhbwheidenheim.informatik.callsim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.dhbwheidenheim.informatik.callsim.controller.SessionHandler;
+
 @SpringBootApplication(exclude = {
 	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class CallSimApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallSimApplication.class, args);
+		SessionHandler.setLogger();
 	}
 }
