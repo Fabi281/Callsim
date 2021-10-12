e3d805df3deb2b420d0e7b26d5b7510d4be61c0a
BlackCreepery <82168641+BlackCreepery@users.noreply.github.com>
2021-10-12 15:45:06 +0200

    Fixed calling someone who is offline or in a call. Removed User left align text

--- client/client/src/main/java/de/callsim/UserItem.java
+++ client/client/src/main/java/de/callsim/UserItem.java
@@ -2,8 +2,6 @@ package de.callsim;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class UserItem extends JPanel {
 
@@ -13,7 +11,6 @@ public class UserItem extends JPanel {
     public UserItem(String name){
         this.name = name;
         userBtn = new JButton(this.name);
-        userBtn.setHorizontalAlignment(SwingConstants.LEFT);
         userBtn.setOpaque(false);
         userBtn.setContentAreaFilled(false);
         userBtn.setBorderPainted(false);
